35b1cf2d82776966287b0d07991911716ab6dd58
ImtiazAhmedAkash <110800373+ImtiazAhmedAkash@users.noreply.github.com>
2022-08-10 22:05:02 +0600

    Update 1005.c

--- Beginner (C)/1005.c
+++ Beginner (C)/1005.c
@@ -1,12 +1,17 @@
 // Average 1
 
 #include <stdio.h>
+
 int main()
 {
     double a, b, result;
+    
     scanf("%lf %lf", &a, &b);
+    
     result = ((a * 3.5) + (b * 7.5)) / 11;
     // result = ((a * w1) + (b * w2)) / (w1+w2);
+    
     printf("MEDIA = %.5lf\n", result);
+    
     return 0;
 }
