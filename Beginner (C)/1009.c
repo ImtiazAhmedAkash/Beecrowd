86dc3b51d3c5a967784efe0eac11fe718555b3a2
ImtiazAhmedAkash <110800373+ImtiazAhmedAkash@users.noreply.github.com>
2022-08-10 22:13:41 +0600

    Update 1009.c

--- Beginner (C)/1009.c
+++ Beginner (C)/1009.c
@@ -1,11 +1,14 @@
 // Salary with Bonus
 
 #include <stdio.h>
+
 int main()
 {
     char name[30];
     double fixed_salary, products_sold;
+    
     scanf("%s %lf %lf", &name, &fixed_salary, &products_sold);
     printf("TOTAL = R$ %.2lf\n", fixed_salary + (products_sold * 0.15));
+    
     return 0;
 }
