b167a1b4d286cdecadd3029c63016607afe16a49
ImtiazAhmedAkash <110800373+ImtiazAhmedAkash@users.noreply.github.com>
2022-08-10 22:12:45 +0600

    Update 1007.c

--- Beginner (C)/1007.c
+++ Beginner (C)/1007.c
@@ -1,9 +1,13 @@
 // Difference
+
 #include <stdio.h>
+
 int main()
 {
     int a, b, c, d;
+    
     scanf("%d %d %d %d", &a, &b, &c, &d);
     printf("DIFERENCA = %d\n", ((a * b) - (c * d)));
+    
     return 0;
 }
