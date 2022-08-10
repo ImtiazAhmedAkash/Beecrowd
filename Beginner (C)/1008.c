ca91ab0183be84d9f534b0ec0f6c9b1166dd5a1b
ImtiazAhmedAkash <110800373+ImtiazAhmedAkash@users.noreply.github.com>
2022-08-10 22:13:15 +0600

    Update 1008.c

--- Beginner (C)/1008.c
+++ Beginner (C)/1008.c
@@ -1,12 +1,16 @@
 // Salary
 
 #include <stdio.h>
+
 int main()
 {
     int number, worked_hours;
     double amount_per_hour;
+    
     scanf("%d %d %lf", &number, &worked_hours, &amount_per_hour);
+    
     printf("NUMBER = %d\n", number);
     printf("SALARY = U$ %.2lf\n", (amount_per_hour * worked_hours));
+    
     return 0;
 }
