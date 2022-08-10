17a2a8df290b83ae31422ffbab0f2975c5f99c19
ImtiazAhmedAkash <110800373+ImtiazAhmedAkash@users.noreply.github.com>
2022-08-10 22:14:14 +0600

    Update 1010.c

--- Beginner (C)/1010.c
+++ Beginner (C)/1010.c
@@ -1,15 +1,20 @@
 // Simple Calculate
 
 #include <stdio.h>
+
 int main()
 {
     int code, unit;
     double price, result = 0.00;
+    
     for (int i = 0; i < 2; i++)
     {
         scanf("%d %d %lf", &code, &unit, &price);
+        
         result = result + (unit * price);
     }
+    
     printf("VALOR A PAGAR: R$ %.2lf\n", result);
+    
     return 0;
 }
