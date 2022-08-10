6881eba1209f6917fc8df0de3a1ca37c920ddff8
ImtiazAhmedAkash <110800373+ImtiazAhmedAkash@users.noreply.github.com>
2022-08-10 22:04:37 +0600

    Update 1004.c

--- Beginner (C)/1004.c
+++ Beginner (C)/1004.c
@@ -1,10 +1,13 @@
 // Simple Product
 
 #include <stdio.h>
+
 int main()
 {
     long long int a, b;
+    
     scanf("%lld %lld", &a, &b);
     printf("PROD = %lld\n", a * b);
+    
     return 0;
 }
